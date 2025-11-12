d17b9b9837708448939156337150319bc2d04df2
Priyanka Rajasekaran <Priyankasekar1911@outlook.com>
2025-11-12 10:30:12 +0530

    Assignment 1- Data Type

---
+++ src/language/basics/DataTypes.java
@@ -0,0 +1,80 @@
+package language.basics;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//Temperature of a city in degrees Celsius : 25.5
+		float Temperature = 25.5f;
+		System.out.println("1.Temperature of a city in degree Celsius"+" = "+Temperature);
+		
+// Whether a customer has placed an order : True or False
+		boolean OrderPlaced = true;
+		System.out.println("2.Whether Customer has placed an order : "+ OrderPlaced);
+//Person's phone number : "123-456-7890"
+		long PhoneNumber = 1234567890l;
+		System.out.println("3.Person's Phone number : "+PhoneNumber);
+//Amount of money in a customers's bank account :1000.50
+		float BankBalance = 1000.50f;
+		System.out.println("4.Amount of money in a customers's bank account"+" = "+BankBalance);
+// Person's email address: "john.doe@example.com"
+		String EmailID = "john.doe@example.com";
+		System.out.println("5.Person's email address : "+ EmailID);
+// Coordinates of a location (latitude, longitude): 37.7749, -122.4194
+		double CoordinateA = 37.7749;
+		double CoordinateB= -122.4194;
+		System.out.println("6.Coordinates of a location (latitude, longitude) : "+ CoordinateA+","+CoordinateB);
+//Person's mariatal status : true or false
+		boolean MariatalStatus= false;
+		System.out.println("7.Person's mariatal status : "+ MariatalStatus);
+// Person's occupation : "Softaware Engineer"
+		String Occupation =  "Softaware Engineer";
+		System.out.println("8.Person's occupation : "+ Occupation);
+//Person's favorite colour : "Purple"
+		String Colour =  "Purple";
+		System.out.println("9.Person's favorite colour : "+ Colour);
+// Current year : 2025
+		short Year = 2025;
+		System.out.println("10.Current year : "+ Year);
+//11.Number of followers on a social media platform: 1,000,000
+		int Followers = 1000000;
+		System.out.println("11.Number of followers on a social media platform : " + Followers);
+//	12.Rating of a movie: 7.5
+		float MovieRating = 7.5f;
+		System.out.println("12.Rating of a movie : "+ MovieRating);
+//13.Person's blood type: 'A'
+		char BloodType = 'A';
+		System.out.println("13.Person's blood type: "+ BloodType );
+//14.Title of a book: "To Kill a Mockingbird"
+		String BookTitle ="To Kill a Mockingbird" ;
+		System.out.println("14.Title of a book : "+ BookTitle);
+//15.Number of employees in a company: 500
+		short EmployeeNo = 500;
+		System.out.println("15.Number of employees in a company : "+ EmployeeNo);
+//16.Time of an event: 2:30 PM
+		String Time = "2:30 PM";
+		System.out.println("16.Time of an event : "+ Time);
+//17.Name of a country: "United States"
+		String Country= "United States";
+		System.out.println("17.Name of a country : "+ Country);
+//18.Person's eye color: "Brown"
+		String EyeColour = "Brown";
+		System.out.println("18.Person's eye color : "+ EyeColour);
+//19.Person's birthplace: "New York City"
+		String BirthPlace = "New York City";
+		System.out.println("19.Person's birthplace : "+ BirthPlace);
+//20.Distance between two cities: 200.5
+		float Distance= 200.5f;
+		System.out.println("20.Distance between two cities : "+ Distance);
+		
+		
+/*Observation: 	
+		out of all data type string should start with Capital letter,
+		long's and float's value should end with l and f respectively
+		Numeric value shouldn't have ","*/
+		
+		
+		
+	}
+
+}
